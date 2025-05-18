ce153f834d4701397daf177a00646ee47a46ab3f
Haswanth Tamil <haswanthtamilofficial@gmail.com>
2025-05-18 21:54:34 +0530

    Add routes.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,22 @@
-import { useState } from "react";
-import "./App.css";
+import { useState } from "react"
+import "./styles/App.css"
+import Home from "./pages/Home.jsx"
+import CreatePost from "./pages/CreatePost.jsx"
+import Post from "./pages/Post.jsx"
+import NotFound from "./pages/NotFound.jsx"
+import { Routes, Route } from "react-router-dom"
 
 function App() {
   return (
     <>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create-post" element={<CreatePost />} />
+        <Route path="/post/:id" element={<Post />} />
+        <Route path="*" element={<NotFound />} />\
+      </Routes>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
